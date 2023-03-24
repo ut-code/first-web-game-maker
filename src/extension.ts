e8286ba2338eb2e9747b0b9903f577f4aabd9555
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-03-24 15:32:29 +0900

    Fix typo

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage("Hello VSCode");
+      vscode.window.showInformationMessage("Hello VS Code");
     }
   );
 
