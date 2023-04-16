e99ab49d1fcea5ac7bba78a1d98f270f4f5d081c
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-04-16 10:00:31 +0900

    Add Markdown description

--- src/extension.ts
+++ src/extension.ts
@@ -307,9 +307,21 @@ export function activate(context: vscode.ExtensionContext) {
 
   panel.webview.html = renderMd(`\
 # First Web Game Maker
+
+使い方の説明を Markdown で書けます！
+
+**太字**はこんな感じで書けます。
+
+~~取り消し線~~も書けます。
+
+コードブロックはこんな感じで書けます。なんと、シンタックスハイライトもできます！
+
 \`\`\`js
 document.write("Hello, World!");
 \`\`\`
+
+HTML もこんな感じでかけます。このボタンは、Webview から VS Code にメッセージを送るサンプルです。ボタンを押すと、VS Code のウィンドウにメッセージが表示されます。
+
 <button id="alert">alert</button>
 <script>
   const vscode = acquireVsCodeApi();
