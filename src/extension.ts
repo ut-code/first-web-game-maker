a084f47cf520f104a0aacefd877d6597841035f1
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-05-01 14:20:41 +0900

    Separate commands definition

--- src/extension.ts
+++ src/extension.ts
@@ -22,17 +22,7 @@ function renderMd(text: string) {
   );
 }
 
-import insertHelloWorldAtTop from "./commands/insertHelloWorldAtTop";
-import insertAtCursor from "./commands/insertAtCursor";
-import deleteAfterCursor from "./commands/deleteAfterCursor";
-import {
-  insertDivElementAtCursor,
-  insertButtonElementAtCursor,
-  insertInputElementAtCursor,
-  insertUnorderedListElementAtCursor,
-  insertH1ElementAtCursor,
-  insertParagraphElementAtCursor,
-} from "./commands/htmlCommands";
+import commands from "./commands";
 
 export function activate(context: vscode.ExtensionContext) {
   // const files = vscode.workspace.textDocuments;
@@ -49,93 +39,6 @@ export function activate(context: vscode.ExtensionContext) {
 
   // vscode.workspace.openTextDocument(paths.scriptJs);
   // vscode.workspace.openTextDocument(paths.indexHtml);
-
-  type Command = {
-    name: string;
-    execute: () => void;
-  };
-
-  const commands: Command[] = [
-    {
-      name: "insertHelloWorldAtTop",
-      execute: () => {
-        insertHelloWorldAtTop();
-      },
-    },
-    {
-      name: "insertAtCursor",
-      execute: async () => {
-        const input = await vscode.window.showInputBox({
-          prompt: "挿入する文字列を入力してください",
-        });
-        insertAtCursor(input || "");
-      },
-    },
-    {
-      name: "deleteAfterCursor",
-      execute: async () => {
-        const input = await vscode.window.showInputBox({
-          prompt: "削除する文字数を入力してください",
-          value: "1",
-        });
-        deleteAfterCursor(parseInt(input || "1"));
-      },
-    },
-    {
-      name: "insertDivElementAtCursor",
-      execute: async () => {
-        const input = await vscode.window.showInputBox({
-          prompt: "挿入する `div` 要素の `id` 属性を入力してください",
-        });
-        insertDivElementAtCursor(input || "");
-      },
-    },
-    {
-      name: "insertButtonElementAtCursor",
-      execute: async () => {
-        const input = await vscode.window.showInputBox({
-          prompt: "挿入する `button` 要素の `id` 属性を入力してください",
-        });
-        insertButtonElementAtCursor(input || "");
-      },
-    },
-    {
-      name: "insertInputElementAtCursor",
-      execute: async () => {
-        const input = await vscode.window.showInputBox({
-          prompt: "挿入する `input` 要素の `id` 属性を入力してください",
-        });
-        insertInputElementAtCursor(input || "");
-      },
-    },
-    {
-      name: "insertUnorderedListElementAtCursor",
-      execute: async () => {
-        const input = await vscode.window.showInputBox({
-          prompt: "挿入する `ul` 要素の `id` 属性を入力してください",
-        });
-        insertUnorderedListElementAtCursor(input || "");
-      },
-    },
-    {
-      name: "insertH1ElementAtCursor",
-      execute: async () => {
-        const input = await vscode.window.showInputBox({
-          prompt: "挿入する `h1` 要素の `id` 属性を入力してください",
-        });
-        insertH1ElementAtCursor(input || "");
-      },
-    },
-    {
-      name: "insertParagraphElementAtCursor",
-      execute: async () => {
-        const input = await vscode.window.showInputBox({
-          prompt: "挿入する `p` 要素の `id` 属性を入力してください",
-        });
-        insertParagraphElementAtCursor(input || "");
-      },
-    },
-  ];
 
   context.subscriptions.push(
     ...commands.map((command) =>
