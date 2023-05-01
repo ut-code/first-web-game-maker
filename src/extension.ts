887999536926d111be55a8e6e8fdba5eeb0e1716
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-05-01 14:50:47 +0900

    Separate treeView

--- src/extension.ts
+++ src/extension.ts
@@ -23,7 +23,7 @@ function renderMd(text: string) {
 }
 
 import commands from "./commands";
-import treeData, { TreeItem } from "./treeData/treeData";
+import treeView from "./treeView";
 
 export function activate(context: vscode.ExtensionContext) {
   // const files = vscode.workspace.textDocuments;
@@ -50,40 +50,6 @@ export function activate(context: vscode.ExtensionContext) {
     )
   );
 
-  class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
-    private data: TreeItem[];
-
-    constructor() {
-      this.data = treeData;
-    }
-
-    private _onDidChangeTreeData: vscode.EventEmitter<
-      TreeItem | undefined | void
-    > = new vscode.EventEmitter<TreeItem | undefined | void>();
-    readonly onDidChangeTreeData: vscode.Event<TreeItem | undefined | void> =
-      this._onDidChangeTreeData.event;
-
-    refresh(): void {
-      this._onDidChangeTreeData.fire();
-    }
-
-    getTreeItem(element: TreeItem): vscode.TreeItem {
-      return element;
-    }
-
-    getChildren(element?: TreeItem): Thenable<TreeItem[]> {
-      if (element) {
-        return Promise.resolve(element.children);
-      } else {
-        return Promise.resolve(this.data);
-      }
-    }
-  }
-
-  const treeDataProvider = new TreeDataProvider();
-  const treeView = vscode.window.createTreeView("first-web-game-maker", {
-    treeDataProvider,
-  });
   context.subscriptions.push(treeView);
 
   const panel = vscode.window.createWebviewPanel(
---
+++ src/treeView.ts
@@ -0,0 +1,39 @@
+import * as vscode from "vscode";
+import treeData, { TreeItem } from "./treeData/treeData";
+
+class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
+  private data: TreeItem[];
+
+  constructor() {
+    this.data = treeData;
+  }
+
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    TreeItem | undefined | void
+  > = new vscode.EventEmitter<TreeItem | undefined | void>();
+  readonly onDidChangeTreeData: vscode.Event<TreeItem | undefined | void> =
+    this._onDidChangeTreeData.event;
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire();
+  }
+
+  getTreeItem(element: TreeItem): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: TreeItem): Thenable<TreeItem[]> {
+    if (element) {
+      return Promise.resolve(element.children);
+    } else {
+      return Promise.resolve(this.data);
+    }
+  }
+}
+
+const treeDataProvider = new TreeDataProvider();
+const treeView = vscode.window.createTreeView("first-web-game-maker", {
+  treeDataProvider,
+});
+
+export default treeView;
