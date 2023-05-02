fda30be9afc83a4fcda4452521b949050aa4bda0
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-05-02 09:21:51 +0900

    Revert "Fix error"
    This reverts commit 87c2b67c51dff5426984d0fa39b66e7ad5fb7ebb.

--- src/utils/convertMarkdownToHtml.ts
+++ src/utils/convertMarkdownToHtml.ts
@@ -17,38 +17,13 @@ export default function convertMarkdownToHtml(filePath: string): string {
     throw new Error(`Invalid file type: ${filePath}`);
   }
 
-  //   // import file content
-  //   const fileContent = fs.readFileSync(
-  //     path.join(__dirname, path.join("./../../src", filePath)),
-  //     {
-  //       encoding: "utf-8",
-  //     }
-  //   );
-  const fileContent = `\
-# First Web Game Maker
-
-使い方の説明を Markdown で書けます！
-
-**太字**はこんな感じで書けます。
-
-~~取り消し線~~も書けます。
-
-コードブロックはこんな感じで書けます。なんと、シンタックスハイライトもできます！
-
-\`\`\`js
-document.write("Hello, World!");
-\`\`\`
-
-HTML もこんな感じでかけます。このボタンは、Webview から VS Code にメッセージを送るサンプルです。ボタンを押すと、VS Code のウィンドウにメッセージが表示されます。
-
-<button id="alert">alert</button>
-
-<script>
-const vscode = acquireVsCodeApi();
-document.getElementById("alert").onclick = () =>{
-    vscode.postMessage({type: "alert", message: "Hello, World!"});
-}
-</script>`;
+  // import file content
+  const fileContent = fs.readFileSync(
+    path.join(__dirname, path.join("./../../src", filePath)),
+    {
+      encoding: "utf-8",
+    }
+  );
 
   // convert markdown to html
   const md = new MarkdownIt({
