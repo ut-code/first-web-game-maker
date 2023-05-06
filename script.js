db0755e9851f61c875f4413d55c493a00c98c7b3
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-05-06 12:32:44 +0900

    rename variables

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // 参加者が変更可能な部分
 
 // 迷路のサイズ
-const size = 20;
+const mazeWidth = 20;
 
 // ===========================================
 // initialize
@@ -15,7 +15,7 @@ let createdCookie = 0;
 
 // template #1
 const mazeState = createStandardMaze20();
-// const mazeState = createPlaneMaze(size);
+// const mazeState = createPlaneMaze(mazeWidth);
 
 // ===========================================
 const pacmanState = {
@@ -35,7 +35,8 @@ const ghostState = {
 const tableDiv = document.getElementById("table");
 const nowDirectionDiv = document.getElementById("now-direction");
 const nextDirectionDiv = document.getElementById("next-direction");
-const eatenCookieSpan = document.getElementById("cookie-point");
+const eatenCookieSpan = document.getElementById("eaten-cookie");
+const createdCookieSpan = document.getElementById("created-cookie");
 
 const pacmanEastImage = new Image();
 pacmanEastImage.src = "./img/pacman_east.svg";
@@ -46,14 +47,14 @@ pacmanNorthImage.src = "./img/pacman_north_temp.svg";
 const pacmanSouthImage = new Image();
 pacmanSouthImage.src = "./img/pacman_south_temp.svg";
 
-const enemyEastImage = new Image();
-enemyEastImage.src = "./img/ghost_east.svg";
-const enemyWestImage = new Image();
-enemyWestImage.src = "./img/ghost_west.svg";
-const enemyNorthImage = new Image();
-enemyNorthImage.src = "./img/ghost_north.svg";
-const enemySouthImage = new Image();
-enemySouthImage.src = "./img/ghost_south.svg";
+const ghostEastImage = new Image();
+ghostEastImage.src = "./img/ghost_east.svg";
+const ghostWestImage = new Image();
+ghostWestImage.src = "./img/ghost_west.svg";
+const ghostNorthImage = new Image();
+ghostNorthImage.src = "./img/ghost_north.svg";
+const ghostSouthImage = new Image();
+ghostSouthImage.src = "./img/ghost_south.svg";
 
 // canvas領域を定義
 const canvas = document.getElementById("canvas");
@@ -64,34 +65,34 @@ const ctx = canvas.getContext("2d");
 
 // let lastTime = performance.now();
 const pacmanPosition = { x: 75, y: 75 }; //最初の出現位置を動的に指定するように修正の必要あり
-const enemyPosition = { x: 525, y: 225 };
+const ghostPosition = { x: 525, y: 225 };
 let nextDirection;
 let nowDirection;
 
-let enemyNextDirection;
-let enemyNowDirection;
+let ghostNextDirection;
+let ghostNowDirection;
 
 // 開始
 createMazeState();
 drawContext();
 drawMazeState();
 // drawPacman(pacmanPosition.x, pacmanPosition.y);
-// drawEnemy(100, 100);
+// drawGhost(100, 100);
 movePacman();
 
 // キーボード操作を取得
 document.onkeydown = onKeyDown;
 
-// Enemy の動作
-setEnemyDirection();
+// ghost の動作
+setGhostDirection();
 
 // 入力を元に壁を生成
 function createMazeState() {
   tableDiv.innerHTML = "";
   const table = document.createElement("table");
-  for (let i = 0; i < size / 2; i++) {
+  for (let i = 0; i < mazeWidth / 2; i++) {
     const tr = document.createElement("tr");
-    for (let j = 0; j < size; j++) {
+    for (let j = 0; j < mazeWidth; j++) {
       const td = document.createElement("td");
       td.style.width = "40px";
       td.style.height = "40px";
@@ -138,8 +139,8 @@ function drawPacman(pacmanImage, x, y) {
   // 位置調整　ハードコーディングしない
 }
 
-function drawEnemy(enemyImage, x, y) {
-  ctx.drawImage(enemyImage, x - 30, y - 30, 65, 65);
+function drawGhost(ghostImage, x, y) {
+  ctx.drawImage(ghostImage, x - 30, y - 30, 65, 65);
   // 位置調整　ハードコーディングしない
 }
 
@@ -162,7 +163,7 @@ function drawCookie(i, j) {
 function movePacman() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   const canMove = canMoveFrom(pacmanPosition, pacmanState);
-  const enemyCanMove = canMoveFrom(enemyPosition, ghostState);
+  const ghostCanMove = canMoveFrom(ghostPosition, ghostState);
 
   // パックマンを動かす
   let pacmanImage;
@@ -190,32 +191,32 @@ function movePacman() {
   }
 
   // 敵を動かす
-  let enemyImage;
-  if (enemyNextDirection === "north" && enemyCanMove.north) {
-    enemyNowDirection = "north";
-  } else if (enemyNextDirection === "south" && enemyCanMove.south) {
-    enemyNowDirection = "south";
-  } else if (enemyNextDirection === "east" && enemyCanMove.east) {
-    enemyNowDirection = "east";
-  } else if (enemyNextDirection === "west" && enemyCanMove.west) {
-    enemyNowDirection = "west";
+  let ghostImage;
+  if (ghostNextDirection === "north" && ghostCanMove.north) {
+    ghostNowDirection = "north";
+  } else if (ghostNextDirection === "south" && ghostCanMove.south) {
+    ghostNowDirection = "south";
+  } else if (ghostNextDirection === "east" && ghostCanMove.east) {
+    ghostNowDirection = "east";
+  } else if (ghostNextDirection === "west" && ghostCanMove.west) {
+    ghostNowDirection = "west";
   }
-  if (enemyNowDirection === "north" && enemyCanMove.north) {
-    enemyPosition.y -= 1;
-    enemyImage = enemyNorthImage;
-  } else if (enemyNowDirection === "south" && enemyCanMove.south) {
-    enemyPosition.y += 1;
-    enemyImage = enemySouthImage;
-  } else if (enemyNowDirection === "east" && enemyCanMove.east) {
-    enemyPosition.x += 1;
-    enemyImage = enemyEastImage;
-  } else if (enemyNowDirection === "west" && enemyCanMove.west) {
-    enemyPosition.x -= 1;
-    enemyImage = enemyWestImage;
+  if (ghostNowDirection === "north" && ghostCanMove.north) {
+    ghostPosition.y -= 1;
+    ghostImage = ghostNorthImage;
+  } else if (ghostNowDirection === "south" && ghostCanMove.south) {
+    ghostPosition.y += 1;
+    ghostImage = ghostSouthImage;
+  } else if (ghostNowDirection === "east" && ghostCanMove.east) {
+    ghostPosition.x += 1;
+    ghostImage = ghostEastImage;
+  } else if (ghostNowDirection === "west" && ghostCanMove.west) {
+    ghostPosition.x -= 1;
+    ghostImage = ghostWestImage;
   }
 
-  hitCookie();
-  hitEnemy();
+  checkCookieHit();
+  checkGhostHit();
   judgeGameClear();
 
   drawContext();
@@ -225,13 +226,14 @@ function movePacman() {
     pacmanPosition.x,
     pacmanPosition.y
   );
-  drawEnemy(enemyImage || enemyEastImage, enemyPosition.x, enemyPosition.y);
+  drawGhost(ghostImage || ghostEastImage, ghostPosition.x, ghostPosition.y);
 
   // debug
   nowDirectionDiv.textContent = `now: ${nowDirection}`;
   nextDirectionDiv.textContent = `next: ${nextDirection}`;
 
   eatenCookieSpan.textContent = eatenCookie * 10;
+  createdCookieSpan.textContent = createdCookie * 10;
 
   setTimeout(() => {
     movePacman();
@@ -264,8 +266,8 @@ function onKeyDown(e) {
 
 // 敵の動く方向をランダムに生成
 // ちゃんとそれっぽく動かす必要がある
-function setEnemyDirection() {
-  const canMove = canMoveFrom(enemyPosition, ghostState);
+function setGhostDirection() {
+  const canMove = canMoveFrom(ghostPosition, ghostState);
   const directions = [];
   canMove.north ? directions.push("north") : null;
   canMove.south ? directions.push("south") : null;
@@ -273,11 +275,11 @@ function setEnemyDirection() {
   canMove.west ? directions.push("west") : null;
 
   console.log("directions", directions);
-  enemyNextDirection =
+  ghostNextDirection =
     directions[Math.floor(Math.random() * directions.length)];
   const interval = Math.random() * 2000 + 1000;
   setTimeout(() => {
-    setEnemyDirection();
+    setGhostDirection();
   }, interval);
 }
 
@@ -344,7 +346,7 @@ function canMoveFrom(position, result) {
   return result;
 }
 
-function hitCookie() {
+function checkCookieHit() {
   // pacmanの大きさが相まっていい感じだが、本来ちゃんと当たり判定をやる必要がある
   const centerX = indexX(pacmanPosition.x);
   const centerY = indexY(pacmanPosition.y);
@@ -354,14 +356,14 @@ function hitCookie() {
   }
 }
 
-function hitEnemy() {
+function checkGhostHit() {
   const centerX = indexX(pacmanPosition.x);
   const centerY = indexY(pacmanPosition.y);
 
-  const enemyCenterX = indexX(enemyPosition.x);
-  const enemyCenterY = indexY(enemyPosition.y);
+  const ghostCenterX = indexX(ghostPosition.x);
+  const ghostCenterY = indexY(ghostPosition.y);
 
-  if (centerY === enemyCenterY && centerX === enemyCenterX) {
+  if (centerY === ghostCenterY && centerX === ghostCenterX) {
     if (confirm("Game Over!")) {
       console.log("ok");
       // 処理を停止する必要がある
@@ -385,15 +387,15 @@ function judgeGameClear() {
 // ===================
 // create maze template
 // 外壁のみ
-// function createPlaneMaze(size) {
-//   const mazeState = new Array(size / 2);
-//   mazeState[0] = new Array(size).fill("x");
-//   for (let i = 1; i < size / 2 - 1; i++) {
-//     mazeState[i] = new Array(size).fill("o");
+// function createPlaneMaze(mazeWidth) {
+//   const mazeState = new Array(mazeWidth / 2);
+//   mazeState[0] = new Array(mazeWidth).fill("x");
+//   for (let i = 1; i < mazeWidth / 2 - 1; i++) {
+//     mazeState[i] = new Array(mazeWidth).fill("o");
 //     mazeState[i][0] = "x";
-//     mazeState[i][size - 1] = "x";
+//     mazeState[i][mazeWidth - 1] = "x";
 //   }
-//   mazeState[size / 2 - 1] = new Array(size).fill("x");
+//   mazeState[mazeWidth / 2 - 1] = new Array(mazeWidth).fill("x");
 //   return mazeState;
 // }
 
