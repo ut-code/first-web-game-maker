bd365d83cd986e1e3516d509770a05f25a28d3cb
chvmvd <104971044+chvmvd@users.noreply.github.com>
2023-05-13 08:39:32 +0900

    Remove CSS description

--- src/treeView.ts
+++ src/treeView.ts
@@ -1,10 +1,9 @@
 import toTreeView from "./utils/toTreeView";
 import htmlTreeData from "./contents/treeData/htmlTreeData";
-import cssTreeData from "./contents/treeData/cssTreeData";
 import sugorokuTreeData from "./contents/treeData/sugorokuTreeData";
 import shogiTreeData from "./contents/treeData/shogiTreeData";
 
-const treeData = [htmlTreeData, cssTreeData, sugorokuTreeData, shogiTreeData];
+const treeData = [htmlTreeData, sugorokuTreeData, shogiTreeData];
 
 const treeView = toTreeView(treeData);
 
