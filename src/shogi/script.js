9f1fb48c790c05e85650e6a91517762327936a81
E-nana17 <104970804+E-nana17@users.noreply.github.com>
2023-05-14 00:15:48 +0900

    順序変更

--- src/shogi/script.js
+++ src/shogi/script.js
@@ -26,23 +26,6 @@ const 先手の持ち駒置き場の境界線 = "1px black solid";
 const 後手の持ち駒置き場の境界線 = "1px black solid";
 
 // ===========================================
-// 駒の初期配置
-
-const 初期配置を左右対称にするか = true;
-const 初期配置の敵陣へのコピー = "face";
-const chessInitial = new Map([
-  [new Cell(0, 0), new Rook(PlayerIndex.WHITE)],
-  [new Cell(0, 1), new Knight(PlayerIndex.WHITE)],
-  [new Cell(0, 2), new Bishop(PlayerIndex.WHITE)],
-  [new Cell(0, 3), new King(PlayerIndex.WHITE)],
-  [new Cell(0, 4), new Qween(PlayerIndex.WHITE)],
-  [new Cell(1, 0), new Pawn(PlayerIndex.WHITE)],
-  [new Cell(1, 1), new Pawn(PlayerIndex.WHITE)],
-  [new Cell(1, 2), new Pawn(PlayerIndex.WHITE)],
-  [new Cell(1, 3), new Pawn(PlayerIndex.WHITE)],
-]);
-
-// ===========================================
 // 駒の種類
 
 class King extends IPiece {
@@ -143,6 +126,21 @@ class Pawn extends IPiece {
 // ]);
 
 // ===========================================
+// 駒の初期配置
+
+const 初期配置を左右対称にするか = true;
+const 初期配置の敵陣へのコピー = "face";
+const chessInitial = new Map([
+  [new Cell(0, 0), new Rook(PlayerIndex.WHITE)],
+  [new Cell(0, 1), new Knight(PlayerIndex.WHITE)],
+  [new Cell(0, 2), new Bishop(PlayerIndex.WHITE)],
+  [new Cell(0, 3), new King(PlayerIndex.WHITE)],
+  [new Cell(0, 4), new Qween(PlayerIndex.WHITE)],
+  [new Cell(1, 0), new Pawn(PlayerIndex.WHITE)],
+  [new Cell(1, 1), new Pawn(PlayerIndex.WHITE)],
+  [new Cell(1, 2), new Pawn(PlayerIndex.WHITE)],
+  [new Cell(1, 3), new Pawn(PlayerIndex.WHITE)],
+]);
 
 // for test
 const pieces = [{ name: "" }, { name: "歩" }];
