800e9e8c804577b38fa8389dc6fff60bac6c63a4
E-nana17 <104970804+E-nana17@users.noreply.github.com>
2023-05-13 09:04:16 +0900

    成り駒の追加

--- src/contents/commands/shogiTemplate/shogiPieceTemplates.ts
+++ src/contents/commands/shogiTemplate/shogiPieceTemplates.ts
@@ -1,10 +1,46 @@
 const shogiPieceTemplates = [
+  `class 駒の名前 extends PieceBase {
+  NAME = "駒の名前";
+  SYMBOL = "駒";
+  MOVE = new JumpMove([new Vector(1, 0)], "none");
+}
+`,
   `class 王将 extends PieceBase {
   NAME = "王将";
   SYMBOL = "王";
   IS_ROYAL = true;
   MOVE = new JumpMove([new Vector(1, 0), new Vector(1, 1)], "oct");
 }
+`,
+  `class 飛車 extends PieceBase {
+  NAME = "飛車";
+  SYMBOL = "飛";
+  MOVE = new RunMove(new Map([[new Vector(1, 0), -1]]), "oct");
+}
+class 竜王 extends PieceBase {
+  NAME = "竜王";
+  SYMBOL = "竜";
+  MOVE = new MergedMove(
+    new RunMove(new Map([[new Vector(1, 0), -1]]), "oct"),
+    new JumpMove([new Vector(1, 1)], "fblr")
+  );
+}
+飛車.updatePromotion([[竜王]]);
+`,
+  `class 角行 extends PieceBase {
+  NAME = "角行";
+  SYMBOL = "角";
+  MOVE = new RunMove(new Map([[new Vector(1, 1), -1]]), "fblr");
+}
+class 竜馬 extends PieceBase {
+  NAME = "竜馬";
+  SYMBOL = "馬";
+  MOVE = new MergedMove(
+    new RunMove(new Map([[new Vector(1, 1), -1]]), "fblr"),
+    new JumpMove([new Vector(1, 0)], "oct")
+  );
+}
+角行.updatePromotion([[竜馬]]);
 `,
   `class 金将 extends PieceBase {
   NAME = "金将";
@@ -23,36 +59,28 @@ const shogiPieceTemplates = [
     "lr"
   );
 }
+銀将.updatePromotion([[金将]]);
 `,
   `class 桂馬 extends PieceBase {
   NAME = "桂馬";
   SYMBOL = "桂";
   MOVE = new JumpMove([new Vector(2, 1)], "lr");
 }
+桂馬.updatePromotion([[金将]]);
 `,
   `class 香車 extends PieceBase {
   NAME = "香車";
   SYMBOL = "香";
   MOVE = new RunMove(new Map([[new Vector(1, 0), -1]]), "none");
 }
+香車.updatePromotion([[金将]]);
 `,
-`class 歩兵 extends PieceBase {
+  `class 歩兵 extends PieceBase {
   NAME = "歩兵";
   SYMBOL = "歩";
   MOVE = new JumpMove([new Vector(1, 0)], "none");
 }
-`,
-  `class 飛車 extends PieceBase {
-  NAME = "飛車";
-  SYMBOL = "飛";
-  MOVE = new RunMove(new Map([[new Vector(1, 0), -1]]), "fblr");
-}
-`,
-  `class 角行 extends PieceBase {
-  NAME = "角行";
-  SYMBOL = "角";
-  MOVE = new RunMove(new Map([[new Vector(1, 1), -1]]), "fblr");
-}
+歩兵.updatePromotion([[金将]]);
 `,
   `class King extends PieceBase {
   NAME = "King";
@@ -91,11 +119,11 @@ const shogiPieceTemplates = [
   `class Pawn extends PieceBase {
   NAME = "Pawn";
   SYMBOL = "P";
-  MOVE = new MoveParallelJoint(
+  MOVE = new MergedMove(
     new JumpMove([new Vector(1, 0)], "none", TInteraction.NO_CAPTURE),
     new JumpMove([new Vector(1, 1)], "lr", TInteraction.ONLY_CAPTURE)
   );
-  INITIAL_MOVE = new MoveParallelJoint(
+  INITIAL_MOVE = new MergedMove(
     new RunMove(
       new Map([[new Vector(1, 1), 2]]),
       "none",
