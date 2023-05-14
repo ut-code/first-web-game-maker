59e668fedd83f7dda2a69cbdc63c9a83bc027b59
naka-12 <104970808+naka-12@users.noreply.github.com>
2023-05-14 10:50:30 +0900

    fix pacman description

--- src/commands.ts
+++ src/commands.ts
@@ -14,7 +14,6 @@ const commands: Command[] = [
   deleteAfterCursorCommand,
 
   ...pacmanCommands,
-  ...htmlCommands,
   ...insertSugorokuCommands,
   insertShogiHtmlAtTopCommand,
   insertShogiJsAtTopCommand,
